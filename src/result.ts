1ea87c34d7150213f32affd952471ed8c813284c
Ben <ben399399@naver.com>
2021-12-04 21:19:54 +0900

    refactor: formatted all code to col80

--- src/result.ts
+++ src/result.ts
@@ -1,18 +1,22 @@
-import { TypeApplier, ValueAlias } from './metaType';
+import { TypeApplier, ValueAlias } from './metaType'
 
-type ResultTypeApplier<V extends ResultType, X> = TypeApplier<"resultType", V, X>
+type ResultTypeApplier<V extends ResultType, X> = TypeApplier<
+  'resultType',
+  V,
+  X
+>
 export function compose<A, B, C, D, T1>(...functions: any) {
   return functions.reduce(
-    (a: (x: T1) => Result<A, B>, b: (y: A) => Result<C, D>) =>
-      (value: T1) => {
-        const prevEvalResult = a(value)
-        switch (prevEvalResult.resultType) {
-          case 'Success':
-            return b(prevEvalResult.value)
-          case 'Failure':
-            return prevEvalResult
-        }
-      })
+    (a: (x: T1) => Result<A, B>, b: (y: A) => Result<C, D>) => (value: T1) => {
+      const prevEvalResult = a(value)
+      switch (prevEvalResult.resultType) {
+        case 'Success':
+          return b(prevEvalResult.value)
+        case 'Failure':
+          return prevEvalResult
+      }
+    }
+  )
 }
 
 export function flatMap<A, B>(result: Result<A, B>) {
@@ -24,23 +28,21 @@ export function flatMap<A, B>(result: Result<A, B>) {
   }
 }
 
-export type Success<S> = ResultTypeApplier<"Success", ValueAlias<S>>
+export type Success<S> = ResultTypeApplier<'Success', ValueAlias<S>>
 export function SUCCESS<S>(value: S): Success<S> {
   return {
-    resultType: "Success",
+    resultType: 'Success',
     value: value
   }
 }
 
-export type Failure<F> = ResultTypeApplier<"Failure", ValueAlias<F>>
+export type Failure<F> = ResultTypeApplier<'Failure', ValueAlias<F>>
 export function FAILURE<F>(value: F): Failure<F> {
   return {
-    resultType: "Failure",
+    resultType: 'Failure',
     value: value
   }
 }
 
-export type ResultType = "Success" | "Failure"
-export type Result<S, F> =
-  Success<S>
-  | Failure<F>
+export type ResultType = 'Success' | 'Failure'
+export type Result<S, F> = Success<S> | Failure<F>
